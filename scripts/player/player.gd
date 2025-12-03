c4d88d5f7806490332fffc1b47e020eb46ee4bd3
ignosuke <236490394+ignosuke@users.noreply.github.com>
2025-12-03 18:12:42 -0300

    added marble motion trail, updated level 04, added level metadata

--- scripts/player/player.gd
+++ scripts/player/player.gd
@@ -3,11 +3,15 @@ class_name Player extends Node3D
 @onready var level_manager: LevelManager = %LevelManager
 @onready var marble: Marble = %Marble
 @onready var camera: CameraContainer = %CameraContainer
+@onready var trail_3d: Trail3D = $Trail3D
 
 func _ready() -> void:
 	global_position = level_manager.current_spawnpoint
 	
 	level_manager.level_finished.connect(_on_level_finished)
+
+func _physics_process(_delta: float) -> void:
+	trail_3d.global_position = marble.global_position
 
 func _on_level_finished() -> void:
 	marble.level_finished = true
