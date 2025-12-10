44bf92326865bae630b7840c19427cb5737b4ebc
ddon-dev <don.rigobert0@protonmail.com>
2025-12-10 13:06:37 -0300

    Added Goal Time display in the Level Select Menu when you click on the times.

--- scripts/ui/menus/level_select.gd
+++ scripts/ui/menus/level_select.gd
@@ -15,8 +15,8 @@ const GOLD = preload("uid://djcph6dppquhy")
 # Menus
 @onready var init_menu: VBoxContainer = %InitMenu
 @onready var level_menu: VBoxContainer = %LevelSelectMenu
-@onready var levels_container: GridContainer = %LevelsContainer
-@onready var high_score_container: GridContainer = %HighScoreContainer
+@onready var levels_container: VBoxContainer = %LevelsContainer
+@onready var high_score_container: VBoxContainer = %HighScoreContainer
 @export var menu_slide: AnimationPlayer
 
 # Buttons
@@ -40,6 +40,7 @@ func _ready() -> void:
 			
 		# Level Button
 		var button = Button.new()
+		
 		if level_path.is_tutorial:
 			button.text = "Tutorial %s" % tut_number
 			tut_number += 1
@@ -47,52 +48,68 @@ func _ready() -> void:
 			button.text = "Level %s" % level_number
 			level_number += 1
 		button.flat = true
+		button.custom_minimum_size = Vector2(0, 48)
 		button.pressed.connect(func():
 			sfx_pressed.play()
 			scene_transition.fade_out()
 			await sfx_pressed.finished
 			StageHandler.go_to_level(level_path)
 		)
-		
-		var medal_particles: PackedScene
-		
-		match record.medal_achieved:
-			0:
-				button.icon = NO_MEDAL
-				medal_particles = null
-			1:
-				button.icon = BRONZE
-				medal_particles = BRONZE_MEDAL_PARTICLES
-			2:
-				button.icon = SILVER
-				medal_particles = SILVER_MEDAL_PARTICLES
-			3:
-				button.icon = GOLD
-				medal_particles = GOLD_MEDAL_PARTICLES
-				
-		button.add_theme_constant_override("hseparation", 20)
-		button.add_theme_constant_override("icon_max_width", 40)
-		
-		# Medal Particles
-		
+
 		# Level Record
 		var highscore_seconds: float = record.highscore
-		var record_time = Label.new()
-		record_time.add_theme_font_size_override("font_size", 35)
-		record_time.add_theme_constant_override("shadow_outline_size", 0)
-		record_time.add_theme_constant_override("outline_size", 21)
-		record_time.custom_minimum_size = Vector2(224, 50)
-		record_time.horizontal_alignment = HORIZONTAL_ALIGNMENT_CENTER
+		var record_time = MenuButton.new()
+		
+		record_time.flat = true
+		record_time.custom_minimum_size = Vector2(0, 48)
 		if highscore_seconds == 0.0:
 			record_time.text = "-- : -- : --"
 		else:
 			record_time.text = get_time_formatted(highscore_seconds)
+		record_time.pressed.connect(func():
+			sfx_pressed.play()
+		)
+		# Goal Time Display
+		var popup: PopupMenu = record_time.get_popup()
+		var bronze_icon = BRONZE
+		var silver_icon = SILVER
+		var gold_icon = GOLD
 
+		popup.add_icon_item(gold_icon,
+		"%s" % get_time_formatted(level_path.gold_medal_time),
+		 -1)
+		popup.add_icon_item(silver_icon,
+		"%s" % get_time_formatted(level_path.silver_medal_time),
+		 -1)
+		popup.add_icon_item(bronze_icon,
+		"%s" % get_time_formatted(level_path.bronze_medal_time),
+		 -1)
+		popup.add_theme_constant_override("icon_max_width", 25)
+		# Medals
+		var medal_particles: PackedScene
+		
+		match record.medal_achieved:
+			0:
+				record_time.icon = NO_MEDAL
+				medal_particles = null
+			1:
+				record_time.icon = BRONZE
+				medal_particles = BRONZE_MEDAL_PARTICLES
+			2:
+				record_time.icon = SILVER
+				medal_particles = SILVER_MEDAL_PARTICLES
+			3:
+				record_time.icon = GOLD
+				medal_particles = GOLD_MEDAL_PARTICLES
+				
+		record_time.add_theme_constant_override("hseparation", 20)
+		record_time.add_theme_constant_override("icon_max_width", 40)
+		
 		levels_container.add_child(button)
 		high_score_container.add_child(record_time)
 		if medal_particles != null:
 			var particles_instance: GPUParticles2D = medal_particles.instantiate()
-			button.add_child(particles_instance)
+			record_time.add_child(particles_instance)
 			particles_instance.set_position(Vector2(25,25))
 
 func get_time_formatted(elapsed_time: float) -> String:
