582bb795bce4dfb9dcae94d732ad18141f435f2a
ddon-dev <don.rigobert0@protonmail.com>
2025-12-10 11:23:28 -0300

    Added particles to the medals displayed in the menu.

--- scripts/ui/menus/level_select.gd
+++ scripts/ui/menus/level_select.gd
@@ -22,6 +22,13 @@ const GOLD = preload("uid://djcph6dppquhy")
 # Buttons
 @onready var return_menu: Button = %Return
 
+# Particles
+const BRONZE_MEDAL_PARTICLES = preload("uid://w0cjamgqgnqa")
+const SILVER_MEDAL_PARTICLES = preload("uid://h6dl01x634ud")
+const GOLD_MEDAL_PARTICLES = preload("uid://bhsayiyv3bub6")
+
+
+
 
 func _ready() -> void:
 	return_menu.pressed.connect(go_back)
@@ -30,6 +37,7 @@ func _ready() -> void:
 	for level_path in StageHandler.levels:
 		var record: PlayerRecord = SaveManager.get_player_record(
 			level_path.level_path)
+			
 		# Level Button
 		var button = Button.new()
 		if level_path.is_tutorial:
@@ -45,33 +53,47 @@ func _ready() -> void:
 			await sfx_pressed.finished
 			StageHandler.go_to_level(level_path)
 		)
+		
+		var medal_particles: PackedScene
+		
 		match record.medal_achieved:
 			0:
 				button.icon = NO_MEDAL
+				medal_particles = null
 			1:
 				button.icon = BRONZE
+				medal_particles = BRONZE_MEDAL_PARTICLES
 			2:
 				button.icon = SILVER
+				medal_particles = SILVER_MEDAL_PARTICLES
 			3:
 				button.icon = GOLD
+				medal_particles = GOLD_MEDAL_PARTICLES
 				
 		button.add_theme_constant_override("hseparation", 20)
 		button.add_theme_constant_override("icon_max_width", 40)
+		
+		# Medal Particles
+		
 		# Level Record
 		var highscore_seconds: float = record.highscore
 		var record_time = Label.new()
 		record_time.add_theme_font_size_override("font_size", 35)
 		record_time.add_theme_constant_override("shadow_outline_size", 0)
 		record_time.add_theme_constant_override("outline_size", 21)
-		record_time.custom_minimum_size = Vector2(224, 45)
+		record_time.custom_minimum_size = Vector2(224, 50)
 		record_time.horizontal_alignment = HORIZONTAL_ALIGNMENT_CENTER
 		if highscore_seconds == 0.0:
 			record_time.text = "-- : -- : --"
 		else:
 			record_time.text = get_time_formatted(highscore_seconds)
-		
+
 		levels_container.add_child(button)
 		high_score_container.add_child(record_time)
+		if medal_particles != null:
+			var particles_instance: GPUParticles2D = medal_particles.instantiate()
+			button.add_child(particles_instance)
+			particles_instance.set_position(Vector2(25,25))
 
 func get_time_formatted(elapsed_time: float) -> String:
 	var total_seconds: int = int(elapsed_time)
